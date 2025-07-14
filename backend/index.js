c15768de9b3c4bab3241862d873e80d92490783a
Prriya dash <119881879+PrriyaBandita-2002@users.noreply.github.com>
2025-07-14 19:40:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://oj-project-xi.vercel.app", // your frontend domain
+    credentials: true, // allow cookies
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
