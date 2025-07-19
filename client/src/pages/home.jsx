37d617ab148913a7ac867834a9cf25cb6153cf90
Prriya dash <119881879+PrriyaBandita-2002@users.noreply.github.com>
2025-07-20 01:36:19 +0530

    Update home.jsx

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -27,9 +27,9 @@ int main() {
     C: `#include <stdio.h>
 
 int main() {
-    int num1, num2;
-    scanf("%d %d", &num1, &num2);
-    printf("Sum: %d\n", num1 + num2);
+    int n1, n2;
+    scanf("%d %d", &n1, &n2);
+    printf("Sum:%d\n",n1 + n2);
     return 0;
 }`,
     Java: `import java.util.Scanner;
